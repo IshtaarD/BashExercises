b1b4cd1b3ac93f277fceee3440d1a4df982e20d5
ishtaar <ishtaar.desravines@gmail.com>
2022-10-20 00:49:20 -0400

    added Script5.sh to get real numbers from the user

---
+++ Bash_Scripting_Exercise_5/Script5.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+echo " REAL NUMBER CALCULATOR"
+echo " ----------------------------"
+
+# Get two real numbers from the user. 
+read -p  "You can add any two real numbers with this calculator! Enter your first real number:   " num1;
+echo $num1
+
+read -p "Enter your second real number:      " num2; 
+echo $num2 
+
+# Check that the number is not a complex number or imaginary number. 
+
+
+
+
+
+# Check if the numbers are being divided (/), only the integer will output when dividing decimal numbers. 
