82d4825951983b5678681a893706422f7c324327
ishtaar <ishtaar.desravines@gmail.com>
2022-10-21 05:46:58 -0400

    added a function to check the user's numbers

--- Bash_Scripting_Exercise_5/Script5.sh
+++ Bash_Scripting_Exercise_5/Script5.sh
@@ -1,14 +1,28 @@
-#!bin/bash
+#!/bin/bash
 
 echo " REAL NUMBER CALCULATOR"
 echo " ----------------------------"
+
+#function to check if the number is a complex or imaginary number
+checkIfRealNum () { 
+    if [[ $1 == *'i'* ]];
+    then
+        echo "Your number is not a real number..."
+    fi
+}
+
 
 # Get two real numbers from the user. 
 read -p  "You can add any two real numbers with this calculator! Enter your first real number:   " num1;
 echo $num1
 
+checkIfRealNum $num1
+
+
 read -p "Enter your second real number:      " num2; 
 echo $num2 
+
+checkIfRealNum $num2
 
 # Check that the number is not a complex number or imaginary number. 
 
@@ -17,3 +31,11 @@ echo $num2
 
 
 # Check if the numbers are being divided (/), only the integer will output when dividing decimal numbers. 
+
+
+
+
+#if [[ $num1 == *"i"* ]];
+#then 
+    #echo "not a real number!"
+#fi
